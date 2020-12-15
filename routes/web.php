854b9bb21fccaa2ab40a1fb1807a0cd407a805b0
why <it06808@163.com>
2020-12-15 10:29:08 +0800

    添加路由文件

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,48 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('welcome', ['website' => 'Laravel 学院！']);
+});*/
+
+Route::view('/', 'welcome', ['website' => 'Why']);
+
+Route::namespace('Home')->get('login', 'PublicController@login');
+
+Route::get('hello/{name}', function(){
+    return view('hello');
 });
+
+Route::get('url/name', function(){
+    return 'My Url ' . route('profile');
+})->name('profile');
+
+Route::get('redirect', function(){
+    //return redirect()->route('trade_info', ['id'=>2]);
+    return redirect()->route('admin.users', ['id'=>2]);
+});
+
+Route::name('admin.')->group(function () {
+    Route::get('users', function () {
+        //echo '路由名称前缀';die;
+        return 'My Url ' . route('users');
+        // 新的路由名称为 "admin.users"...
+    })->name('users');
+});
+
+
+Route::get('trade/index', 'Home\TradeController@index');
+
+Route::get('trade/info/{id}/{fields?}', 'Shop\TradeController@info')->name('trade_info')->middleware('login');
+
+Route::get('user/info', 'Shop\UserController@info');
+
+Route::get('public/info', 'Home\PublicController@info');
+
+Route::namespace('Home')->group(function(){
+    Route::get('lg', 'PublicController@login');
+});
+
+/*Route::group(['namespace' => 'Home'], function(){
+    Route::get('login', 'PublicController@login')->name('unsubscribe');
+});*/
